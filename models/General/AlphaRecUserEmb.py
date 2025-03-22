433d9e83903f0087c25f2a12d9f7c4edc2b484a7
Andrei Chernov <andrei.chernov@flix.com>
2025-03-23 00:20:33 +0100

    added GG MoE for user emb

--- models/General/AlphaRecUserEmb.py
+++ models/General/AlphaRecUserEmb.py
@@ -143,9 +143,9 @@ class AlphaRecUserEmb(AbstractModel):
             #     nn.LeakyReLU(),
             #     nn.Linear(self.multiplier_user_embed_dim*self.emb_dim,  self.embed_size)
             self.mlp_user = MoE(d_in=self.multiplier_user_embed_dim * self.embed_size, d_out=self.embed_size,
-                                n_blocks=1, d_block=4*self.multiplier_user_embed_dim * self.embed_size,
-                                dropout=None, activation='LeakyReLU', gating_type='gumbel',
-                                d_block_per_expert=self.multiplier_user_embed_dim * self.embed_size // 2,
+                                n_blocks=1, d_block=8*self.multiplier_user_embed_dim * self.embed_size,
+                                dropout=0.2, activation='LeakyReLU', gating_type='gumbel',
+                                d_block_per_expert=self.multiplier_user_embed_dim * self.embed_size,
                                 default_num_samples=10, tau=1.0)
 
         print('mlp:')
