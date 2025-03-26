1ee692401db230f5ed6f85ffa7dbd1e9484a1b76
Andrei Chernov <andrei.chernov@flix.com>
2025-03-26 21:31:25 +0100

    mlp items to moe items

--- models/General/AlphaRecUserEmb.py
+++ models/General/AlphaRecUserEmb.py
@@ -181,14 +181,14 @@ class AlphaRecUserEmb(AbstractModel):
             # )
             self.mlp = MoE(d_in=self.init_embed_shape,
                            d_out=self.embed_size,
-                           n_blocks=2,
-                           d_block=4*int(multiplier * self.init_embed_shape),
+                           n_blocks=1,
+                           d_block=8*int(multiplier * self.init_embed_shape),
                            dropout=None,
                            activation='LeakyReLU',
                            num_experts=32,
                            gating_type='gumbel',
                            default_num_samples=10,
-                           tau=2.0)
+                           tau=0.1)
 
         if self.user_model_version == 'homo':
             self.mlp_user = nn.Sequential(
